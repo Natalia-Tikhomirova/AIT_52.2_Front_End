193800cb69e8bcccd047dc1efa1a793708444a9d
Natalia Tikhomirova <ntikhomirova@gmail.com>
2024-12-09 21:26:55 +0100

    Upd hw 13

--- Homeworks/homework_13/script.js
+++ Homeworks/homework_13/script.js
@@ -13,30 +13,37 @@ let bankAccount = {
     accountHolderName: "Alice",
     balance: 0,
 
-// deposit(sum) { TODO Пополнение счёта }
+    // deposit(sum) { TODO Пополнение счёта }
 
-deposit(sum){
-    this.balance += sum;
-    console.log(`Счет пополнен на ${sum}.`);
-},
+    deposit(sum) {
+        this.balance += sum;
+        console.log(`Счет пополнен на ${sum}.`);
+    },
 
-// withdraw(sum) { TODO Списание счёта }
+    // withdraw(sum) { TODO Списание счёта }
+    withdraw(sum) {
+        if (sum <= 0) {
+            console.log("Сумма должна быть больше нуля.");
+            return;
+        }
+        if (sum > this.balance) {
+            console.log("Недостаточно средств на счёте.");
+            return;
+        }
+    
+        // Если проверки пройдены, выполняем списание
+        this.balance -= sum;
+        console.log(`Со счёта списано ${sum}.`);
+    },
 
-withdraw(sum){
-
-    this.balance -= sum;
-    console.log (`Со счета списано ${sum}.`);
-},
-
-// checkBalance() { TODO Просмотр баланса счёта }
-
-checkBalance(){
-
-    console.log(`Текущий баланс ${this.balance}`);
-}
-
+    // checkBalance() { TODO Просмотр баланса счёта }
+    checkBalance() {
+        console.log(`Текущий баланс: ${this.balance}`);
+    }
 };
+
 
 bankAccount.deposit(1000);
 bankAccount.withdraw(600);
 bankAccount.checkBalance();
+bankAccount.withdraw(500);   // Пробуем снять больше, чем есть
