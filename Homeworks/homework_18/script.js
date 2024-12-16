48f56ed52edc9ae4ab8bbaec8921bb100779e74b
Natalia Tikhomirova <ntikhomirova@gmail.com>
2024-12-16 16:46:02 +0100

    upd

--- Homeworks/homework_18/script.js
+++ Homeworks/homework_18/script.js
@@ -2,20 +2,23 @@
 // выведите картинку, status, name, origin (из какой локации этот персонаж)
 
 
-const img = document.querySelector('#morty-img')
-const nameElement = document.querySelector ('#morty-name')
-const statusElement = document.querySelector('#morty-status')
-const originElement = document.querySelector('#morty-origin')
+const img = document.querySelector('#morty-img');
+const nameElement = document.querySelector('#morty-name');
+const statusElement = document.querySelector('#morty-status');
+const originElement = document.querySelector('#morty-origin');
+
+
 
 fetch("https://rickandmortyapi.com/api/character")
   // первым .then() дожидаемся 'сырых' данных и преобразуем их в JS объект с помощью .json()
   .then(res => res.json())
   .then(data => {
     console.log(data.results[1].image);
-    img.src = data.results[1].image;
-    nameElement.textContent = data.nameElement;
-    statusElement.textContent = data.statusElement;
-    originElement.textContent = data.originElement;
+    const character = data.results[1];
+    img.src = character.image;
+    nameElement.textContent = character.name; 
+    statusElement.textContent = character.status; 
+    originElement.textContent = character.origin.name; 
 
    }); 
   
