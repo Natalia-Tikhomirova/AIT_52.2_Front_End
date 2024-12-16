52c34295cfe02b89594e1efe3fbb8e3625cfe3f4
Natalia Tikhomirova <ntikhomirova@gmail.com>
2024-12-16 14:04:36 +0100

    Upd

---
+++ Lessons/lesson_18/example1/script.js
@@ -0,0 +1,29 @@
+// ! теория
+
+// HTTP - HyperText Transfer Protocol - протокол передачи гипертекста
+// API - Application Programming Interface - интерфейс для 'общения' программ друг с другом
+
+// * делаем fetch запрос за данными к API (частный случай работы с сервером)
+
+// * в ответ на fetch запрос приходит объект Promise в состоянии <Pending>
+// * его не получится обработать синхронно - нужно использовать методы работы с асинхронными данными
+
+// const data = fetch('https://dog.ceo/api/breeds/image/random')
+// console.log(data)
+// * ответ с сервера мы получаем в формате JSON
+// JSON - JavaScript Object Notation
+
+// ! делаем запрос
+
+// находим пустой элемент img, чтобы добавить в него данные из API
+const img = document.querySelector('#dog-img')
+
+fetch("https://dog.ceo/api/breeds/image/random")
+  // первым .then() дожидаемся 'сырых' данных и преобразуем их в JS объект с помощью .json()
+  .then(res => res.json())
+  // в data уже придут обработанные данные готовые к использованию
+  .then(data => {
+    // перезаписываем аттрибут src значением из data
+    img.src = data.message
+  });
+
---
+++ Lessons/lesson_18/example2/script.js
@@ -0,0 +1,27 @@
+// отобразить данные по выбранному персонажу из API на странице
+// выведите картинку, status, name, origin (из какой локации этот персонаж)
+
+
+const img = document.querySelector('#morty-img')
+const nameElement = document.querySelector ('#morty-name')
+const statusElement = document.querySelector('#morty-status')
+const originElement = document.querySelector('#morty-origin')
+
+fetch("https://rickandmortyapi.com/api/character")
+  // первым .then() дожидаемся 'сырых' данных и преобразуем их в JS объект с помощью .json()
+  .then(res => res.json())
+  .then(data => {
+    console.log(data.results[1].image);
+    img.src = data.results[1].image;
+    nameElement.textContent = data.nameElement;
+    statusElement.textContent = data.statusElement;
+    originElement.textContent = data.originElement;
+
+   }); 
+  
+     
+  
+
+ 
+  
+  
